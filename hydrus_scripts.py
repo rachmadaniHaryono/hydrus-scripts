3cfa62438fb56db898e1833f843bd0da69f91cf0
Rachmadani Haryono <foreturiga@gmail.com>
2021-09-06 08:58:37 +0800

    new: dev: yaml config

--- hydrus_scripts.py
+++ hydrus_scripts.py
@@ -4,6 +4,7 @@
 """personal hydrus scripts
 """
 import click
+import yaml
 
 __author__ = """rachmadani haryono"""
 __email__ = """foreturiga@gmail.com"""
@@ -12,14 +13,30 @@ __version__ = """0.1.0"""
 
 @click.group()
 @click.version_option(__version__)
+@click.pass_context
+@click.argument("config_yaml", type=click.Path(exists=True))
 @click.option("--debug/--no-debug", default=False)
-def main(debug):
+def main(ctx, config_yaml, debug):
     click.echo(f"Debug mode is {'on' if debug else 'off'}")
+    if debug:
+        click.echo(click.format_filename(config_yaml))
+    with open(config_yaml) as f:
+        config = yaml.safe_load(f)
+    # ensure that ctx.obj exists and is a dict (in case `cli()` is called
+    # by means other than the `if` block below)
+    ctx.ensure_object(dict)
+
+    ctx.obj["DEBUG"] = debug
+    ctx.obj["CONFIG"] = config
 
 
 @main.command()
-def sync():
-    click.echo("Syncing")
+@click.pass_context
+def print_access_key(ctx):
+    try:
+        click.echo(f"access_key: {ctx.obj['CONFIG']['access_key']}")
+    except Exception:
+        click.echo("error:no access_key")
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = [
-    "click",
-]
+requirements = ["click", "pyyaml"]
 
 test_requirements = []
 
